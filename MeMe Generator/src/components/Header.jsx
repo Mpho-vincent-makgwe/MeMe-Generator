d7e1839f2632764cac85d4474f41d50682d79598
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-07-13 14:05:07 +0200

    Creayed meme

---
+++ MeMe Generator/src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return(
+        <header className="header">
+            <img src="../Images/troll-face.png"/>
+            <h2>MeMe Gen by Mpho</h2>
+            <h4>React Course - 3'rd Project</h4>
+        </header>
+        
+    ) 
+}
